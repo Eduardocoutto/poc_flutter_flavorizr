5820bf2fa1c08daf932658240c7c53e1dad3d8c9
Eduardo Rodrigues <eduardo@ephealth.com.br>
2021-07-15 21:04:41 -0300

    Adiciona segundo projeto no firebase

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:poc_flavor/app.dart';
 
-import 'package:poc_flavor/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
